52521dd40a630f1f62c48f63ce24c46bedfe60ea
rifat-sultana <rifatsultana88.web@gmail.com>
2026-04-15 07:02:38 +0600

    complete navbar functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import KeenKeeper from "../../assets/KeenKeeper.png"
 import Home from "../../assets/Home.png";
 import Clock from "../../assets/Clock.png";
@@ -18,24 +19,44 @@ const Navbar = () => {
 
      
         <div className="flex items-center px-8 gap-4">
-          
-          <Link
-            to="/"
-            className="flex items-center gap-1 bg-green-800 text-white px-4 py-1.5 rounded-md text-sm">
-           <img src={Home} alt="Home"  /> Home
-          </Link>
 
-          <Link
-            to="/timeline"
-            className="flex items-center gap-1 text-gray-600 hover:text-black text-sm">
-            <img src={Clock} alt="Clock"/>Timeline
-          </Link>
+          {/* Home  */}
+         <NavLink
+          to="/"
+            className={({ isActive }) =>
+            `flex items-center gap-1 px-4 py-1.5 rounded-md text-sm ${
+             isActive ? "bg-green-800 text-white" : "text-gray-600"
+             }` }>
 
-          <Link
-            to="/stats"
-            className="flex items-center gap-1 text-gray-600 hover:text-black text-sm" >
-            <img src={Stats} alt="Stats"  /> Stats
-          </Link>
+            <img src={Home} alt="Home" />
+              Home
+              </NavLink>
+
+              {/* Timeline */}
+
+          <NavLink
+          to="/timeline"
+            className={({ isActive }) =>
+           `flex items-center gap-1 px-4 py-1.5 rounded-md text-sm ${
+            isActive ? "bg-green-800 text-white" : "text-gray-600"
+           }` }>
+
+            <img src={Clock} alt="Clock" />
+              Timeline
+                </NavLink>
+
+                {/* Stats */}
+
+          <NavLink
+          to="/stats"
+            className={({ isActive }) =>
+            `flex items-center gap-1 px-4 py-1.5 rounded-md text-sm ${
+            isActive ? "bg-green-800 text-white" : "text-gray-600"
+            }` }>
+
+            <img src={Stats} alt="Stats" />
+              Stats
+               </NavLink>
 
         </div>
       </div>

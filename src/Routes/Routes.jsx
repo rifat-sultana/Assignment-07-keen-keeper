7009617bac57278d148bb06ed4199592458c8f9b
rifat-sultana <rifatsultana88.web@gmail.com>
2026-04-17 17:12:09 +0600

    final fix all]

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../Layout/mainLayout";
+import MainLayout from "../Layout/MainLayout";
 import Friends from "../Pages/friends";
 import Timeline from "../Pages/Timeline";
 import StatsData from "../components/StatsData/StatsData";
